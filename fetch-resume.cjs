57864aa663ddc363e344cb8ddb2a65804557f3d8
Ryan Groch <ryan.w.groch@gmail.com>
2025-06-24 08:10:13 -0500

    build: switches resume to the release from Resume-Builder

--- fetch-resume.cjs
+++ fetch-resume.cjs
@@ -1,14 +1,8 @@
 const fs = require("fs");
 
 (async () => {
-  const headers = new Headers();
-  headers.append("Accept", "application/vnd.github.v3.raw");
-
   const pdf_request = await fetch(
-    "https://api.github.com/repos/ryangroch/resume/contents/resume.pdf",
-    {
-      headers,
-    },
+    "https://github.com/RyanGroch/Resume-Builder/releases/latest/download/resume.pdf",
   );
 
   const pdf = await pdf_request.bytes();
